b1a96614b60b07d7c5b8c1109ff81c1b14eb1a23
Avinash Ratnam <avinashr@umich.edu>
2023-11-19 04:51:28 -0500

    Checked out carbon emission

--- final_avinashr.py
+++ final_avinashr.py
@@ -1,5 +1,7 @@
 import json
 import requests
+from bs4 import BeautifulSoup
+from termcolor import colored
 from Secrets import API_KEY
 
 
@@ -39,10 +41,21 @@ def getDirections(route):
     This function takes in the route as a list of tuples and prints out the
     directions.
     """
+    directions = []
     print("\nDIRECTIONS:\n")
     for step in route:
-        print(step)
-    print("\n")
+        directions.append(step)
+    return directions
+
+
+def showDirections(directions):
+    """
+    This function takes in the directions as a list of tuples and prints out
+    the directions.
+    """
+    for step in directions:
+        soup = BeautifulSoup(step, "html.parser")
+        print(soup.text)
 
 
 def getDistance(start, end):
@@ -84,17 +97,36 @@ def getDuration(start, end):
     data = json.loads(response.text)
     for step in data["routes"][0]["legs"][0]["steps"]:
         duration += step["duration"]["value"]
+    duration = duration / 120
+    duration = round(duration, 2)
     return duration
+
+
+def getCarbonEmission(distance):
+    """
+    This function takes in the distance of the route and returns the carbon
+    emission of the route using the EPA API
+    """
+    url = (
+        "https://api.triptocarbon.xyz/v1/footprint?activity="
+        + distance
+        + "&activityType=miles&country=usa&mode=bus&fuelType=gasoline&"
+        + "vehicleEfficiency=20&isPublicTransport=false&planeSeatClass=economy"
+    )
+    response = requests.get(url)
+    data = json.loads(response.text)
+    carbonEmission = data["carbonFootprint"]
+    return carbonEmission
 
 
 def __main__():
     start, end = getStartAndEndPoints()
     route = getRoute(start, end)
-    getDirections(route)
+    showDirections(getDirections(route))
     distance = getDistance(start, end)
     duration = getDuration(start, end)
     print("The total distance of the route is: " + str(distance) + " meters")
-    print("The total duration of the route is: " + str(duration) + " seconds")
+    print("The total duration of the route is: " + str(duration) + " hour(s)")
 
 
 if __name__ == "__main__":
